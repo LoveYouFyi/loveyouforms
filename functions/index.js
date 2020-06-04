5e722a624ad1955cff97cc3b8e5c2835e8c93416
NEH <erik@bodigiti.com>
2020-06-04 12:34:12 -0400

    1.0.3 - update.

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,28 @@
-
+/*------------------------------------------------------------------------------
+  LoveYouForms is a cloud app that handles form submissions for one website 
+  or 1,000 using Google's Firebase platform and its Firestore database.
+  IMPORTANT: for database setup -> follow the instructions at loveyouforms.com 
+------------------------------------------------------------------------------*/
 const loveyouforms = require('loveyouforms');
 
+/*------------------------------------------------------------------------------
+  Form-Handler HTTP Firebase Cloud Function
+  Handles data sent by html form submission, validates allowed form fields and 
+  creates Firestore database entry with the allowed fields.
+------------------------------------------------------------------------------*/
 exports.formHandler = loveyouforms.formHandler;
+
+/*------------------------------------------------------------------------------
+  Firestore-to-Sheets Trigger Firebase Cloud Function
+  Syncs Firestore database 'submitForm' entries to google sheets.
+  If html form does not have matching sheet, creates new sheet/tab & row header.
+------------------------------------------------------------------------------*/
 exports.firestoreToSheets = loveyouforms.firestoreToSheets;
 
+/*------------------------------------------------------------------------------
+  Doc-Schema Trigger Firebase Cloud Functions
+  When Firestore database 'doc' of collection type 'app' or 'formTemplate' is 
+  created, this adds default fields/schema to it. 
+------------------------------------------------------------------------------*/
 exports.schemaApp = loveyouforms.schemaApp;
 exports.schemaFormTemplate = loveyouforms.schemaFormTemplate;
