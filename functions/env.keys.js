fa1fad5b8519279cb122e830ee35e0360b24b9a4
NEH <erik@bodigiti.com>
2020-06-26 08:12:04 -0400

    IP Address: Add 'public-ip' module and return myIp() ip v4 or v6 --> function working for v4 and have else/if and if for v6 --> how to test v6?

--- functions/env.keys.js
+++ functions/env.keys.js
@@ -1,3 +1,35 @@
+const publicIp = require('public-ip');
+
+/*
+(async () => {
+
+  if (await publicIp.v4()) {
+    myIp = await publicIp.v4()
+  } else if (await publicIp.v6()) {
+    myIp = await publicIp.v6()
+  }
+})();
+*/
+
+const myIp = async () => {
+  const v4 = await publicIp.v4();
+  console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ ", v4);
+
+  if (v4) {
+    return v4;
+  } else if (!v4) {
+    const v6 = await publicIp.v6();
+    if (v6) {
+      console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ ", v6);
+      return v6;
+    }
+  } else {
+    console.info("IP Address not found!")
+  }
+}
+//ip();
+myIp();
+
 
 const devKeys = {
   loveyouforms: './dev/loveyouforms-package',
